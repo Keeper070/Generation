6282ec4bfcacedb6338089267d49c9cd3759104f
Keeper070 <99165594+Keeper070@users.noreply.github.com>
2023-03-25 15:51:34 +0400

    add const

--- Generation/Form1.cs
+++ Generation/Form1.cs
@@ -7,18 +7,31 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
 
 namespace Generation
 {
     public partial class Form1 : Form
     {
+        private enum RandomTypeEnum
+        {
+            Лемер, Встроенный
+        }
         private Mathematic _mathematic;
         private bool _choiseGeneration = true;
+        private bool _choiseGeneratorLemer = false;
+        private RandomTypeEnum _type;
 
         public Form1()
         {
-            _mathematic = new Mathematic(this);
             InitializeComponent();
+            _mathematic = new Mathematic(this);
+            this.chart1.Palette = ChartColorPalette.Bright;
+            this.chart1.Titles.Add("f(x)");
+            this.chart1.Series.RemoveAt(0);
+            this.chart2.Palette = ChartColorPalette.Bright;
+            this.chart2.Titles.Add("F(x)");
+            this.chart2.Series.RemoveAt(0);
         }
 
       
@@ -30,11 +43,14 @@ namespace Generation
         private void radioButton1_CheckedChanged(object sender, EventArgs e)
         {
             _choiseGeneration = true;
+            _type = RandomTypeEnum.Встроенный;
         }
 
         private void radioButton2_CheckedChanged(object sender, EventArgs e)
         {
             _choiseGeneration = false;
+            _choiseGeneratorLemer = true;
+            _type = RandomTypeEnum.Лемер;
         }
         
         private void buttonStart_Click(object sender, EventArgs e)
@@ -44,7 +60,19 @@ namespace Generation
                 double[] buffArr = _mathematic.ButtonStart((int)numericSequence.Value);
                 listView2.Items.Add((listView2.Items.Count + 1 + ") ") + "Mx = " + buffArr[0].ToString("#.###") + "\n" +
                                     " Dx = " + buffArr[1].ToString("#.###"));
-             
+                // Series series1=this.chart1.Series.Add(listView1.Items.Count.ToString() + " - " + numericSequence.Value + " - " + type);
+                // Series series2 = this.chart2.Series.Add(listView1.Items.Count.ToString() + " - " + numericSequence.Value + " - " + type);
+                // series2.ChartType = SeriesChartType.Line;
+                // for (int i = 0; i < 100; i++)
+                // {
+                //     series1.Points.Add(_mathematic.ar);
+                //     series2.Points.AddXY(i + 1, mathObject.arrP[i]);
+                //     series2.Points.AddXY(i + 2, mathObject.arrP[i]);
+                // }
+            }
+            else if (_choiseGeneratorLemer)
+            {
+                
             }
         }
 
--- Generation/Mathematic.cs
+++ Generation/Mathematic.cs
@@ -6,9 +6,15 @@ namespace Generation
     public class Mathematic
     {
         private readonly Form1 _form1;
-        private double[] _arrayM;
-        private double[] _arrayP;
+        private double[] _arrayM { get; set; }
+        private double[] _arrayP { get; set; }
         private Random _random;
+
+        #region Const 
+        private const int A = 25173;
+        private const int B = 13849;
+        private const int C = 65536;
+        #endregion
         
         //Длина последовательности
         private int NumericSequence { get; set; }
@@ -24,6 +30,7 @@ namespace Generation
         
         public double[] ButtonStart(int numericSequence)
         {
+            _arrayM = new double[100];
             NumericSequence = numericSequence;
             GenerateArrayM();
             double[] buff = SearchValue();
@@ -65,6 +72,11 @@ namespace Generation
             var disp = mathExpectedDisk2 - mathExpectationDisk1 * mathExpectationDisk1;
 
             return new[] { mathExpectationDisk1, disp };
+        }
+
+        private double Lemer()
+        {
+            
         }
     }
 }
